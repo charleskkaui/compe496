632f888216bdbedacf05a97622e46918d576a581
charleskkaui <60274453+charleskkaui@users.noreply.github.com>
2021-11-07 23:52:07 -0800

    corrected syntax on the print statements

--- bluetooth_test.py
+++ bluetooth_test.py
@@ -4,10 +4,10 @@ port = 1
 server_sock.bind(("",port))
 server_sock.listen(1)
 client_sock,address = server_sock.accept()
-print"Accepted connection from ",address
+print("Accepted connection from ",address)
 while True:
     recvdata =client_sock.recv(1024)
-    print"Received \"%s\" through Bluetooth"%recvdata
+    print("Received \"%s\" through Bluetooth"%recvdata)
     if(recvdata =="Q"):
         print("Exiting")
         break
