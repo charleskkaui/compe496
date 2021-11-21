eb122685544afec1f81f1bbeabc046a213205b0a
charleskkaui <60274453+charleskkaui@users.noreply.github.com>
2021-11-20 23:51:21 -0800

    retrying picamera

--- picameratest.py
+++ picameratest.py
@@ -1,3 +1,18 @@
-import libcamera
+import cv2
+from picamera import PiCamera
+from picamera.array import PiRGBArray
 
-libcamera-hello --tuning-file /usr/share/libcamera/ipa/raspberrypi/imx219_noir.json --qt-preview
+
+camera = PiCamera()
+camera.resolution(640,480)
+camera.framerate = 30
+camera.brightness = 65
+
+raw_footage = PiRGBArray(camera, size(640,480))
+
+for frame in camera.capture_continuous(raw_footage, format="rgb", use_video_port=True):
+    img = frame.array
+    cv2.imshow("Original Image", img)
+    raw_capture.truncate(0)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
