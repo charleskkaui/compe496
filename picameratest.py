795cc03bd835165815ed14406f5ab0f317856ee2
charleskkaui <60274453+charleskkaui@users.noreply.github.com>
2021-11-21 01:11:23 -0800

    yass

--- picameratest.py
+++ picameratest.py
@@ -1,86 +1,123 @@
-import cv2
-import time
-import sys
-import argparse
-import os
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@charleskkaui 
+jeremygblake
+/
+opencv
+Public
+forked from opencv/opencv
+0
+0
+49.1k
+Code
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+opencv/samples/python/facedetect.py /
+@alalek
+alalek fix pylint warnings
+…
+Latest commit 0e40c8a on Oct 16, 2019
+ History
+ 1 contributor
+Executable File  79 lines (60 sloc)  2.18 KB
+   
+#!/usr/bin/env python
 
-def save_snaps(width=0, height=0, name="snapshot", folder=".", raspi=False):
+'''
+face detection using haar cascades
+USAGE:
+    facedetect.py [--cascade <cascade_fn>] [--nested-cascade <cascade_fn>] [<video_source>]
+'''
 
-    if raspi:
-        os.system('sudo modprobe bcm2835-v4l2')
+# Python 2/3 compatibility
+from __future__ import print_function
 
-    cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
-    cv2.VideoWriter_fourcc('F','F','V','1')
+import numpy as np
+import cv2 as cv
 
-    if width > 0 and height > 0:
-        print("Setting the custom Width and Height")
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-    try:
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-            # ----------- CREATE THE FOLDER -----------------
-            folder = os.path.dirname(folder)
-            try:
-                os.stat(folder)
-            except:
-                os.mkdir(folder)
-    except:
-        pass
-
-    nSnap   = 0
-    w       = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    h       = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-
-    fileName    = "%s/%s_%d_%d_" %(folder, name, w, h)
-    while True:
-        ret, frame = cap.read()
-
-        cv2.imshow('camera', frame)
-
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            break
-        if key == ord(' '):
-            print("Saving image ", nSnap)
-            cv2.imwrite("%s%d.jpg"%(fileName, nSnap), frame)
-            nSnap += 1
-
-    cap.release()
-    cv2.destroyAllWindows()
-
+# local modules
+from video import create_capture
+from common import clock, draw_str
 
 
+def detect(img, cascade):
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30),
+                                     flags=cv.CASCADE_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    rects[:,2:] += rects[:,:2]
+    return rects
+
+def draw_rects(img, rects, color):
+    for x1, y1, x2, y2 in rects:
+        cv.rectangle(img, (x1, y1), (x2, y2), color, 2)
 
 def main():
-    # ---- DEFAULT VALUES ---
-    SAVE_FOLDER = "."
-    FILE_NAME = "snapshot"
-    FRAME_WIDTH = 0
-    FRAME_HEIGHT = 0
+    import sys, getopt
 
-    # ----------- PARSE THE INPUTS -----------------
-    parser = argparse.ArgumentParser(
-        description="Saves snapshot from the camera. \n q to quit \n spacebar to save the snapshot")
-    parser.add_argument("--folder", default=SAVE_FOLDER, help="Path to the save folder (default: current)")
-    parser.add_argument("--name", default=FILE_NAME, help="Picture file name (default: snapshot)")
-    parser.add_argument("--dwidth", default=FRAME_WIDTH, type=int, help="<width> px (default the camera output)")
-    parser.add_argument("--dheight", default=FRAME_HEIGHT, type=int, help="<height> px (default the camera output)")
-    parser.add_argument("--raspi", default=False, type=bool, help="<bool> True if using a raspberry Pi")
-    args = parser.parse_args()
+    args, video_src = getopt.getopt(sys.argv[1:], '', ['cascade=', 'nested-cascade='])
+    try:
+        video_src = video_src[0]
+    except:
+        video_src = 0
+    args = dict(args)
+    cascade_fn = args.get('--cascade', "data/haarcascades/haarcascade_frontalface_alt.xml")
+    nested_fn  = args.get('--nested-cascade', "data/haarcascades/haarcascade_eye.xml")
 
-    SAVE_FOLDER = args.folder
-    FILE_NAME = args.name
-    FRAME_WIDTH = args.dwidth
-    FRAME_HEIGHT = args.dheight
+    cascade = cv.CascadeClassifier(cv.samples.findFile(cascade_fn))
+    nested = cv.CascadeClassifier(cv.samples.findFile(nested_fn))
+
+    cam = create_capture(video_src, fallback='synth:bg={}:noise=0.05'.format(cv.samples.findFile('samples/data/lena.jpg')))
+
+    while True:
+        _ret, img = cam.read()
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        gray = cv.equalizeHist(gray)
+
+        t = clock()
+        rects = detect(gray, cascade)
+        vis = img.copy()
+        draw_rects(vis, rects, (0, 255, 0))
+        if not nested.empty():
+            for x1, y1, x2, y2 in rects:
+                roi = gray[y1:y2, x1:x2]
+                vis_roi = vis[y1:y2, x1:x2]
+                subrects = detect(roi.copy(), nested)
+                draw_rects(vis_roi, subrects, (255, 0, 0))
+        dt = clock() - t
+
+        draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
+        cv.imshow('facedetect', vis)
+
+        if cv.waitKey(5) == 27:
+            break
+
+    print('Done')
 
 
-    save_snaps(width=args.dwidth, height=args.dheight, name=args.name, folder=args.folder, raspi=args.raspi)
-
-    print("Files saved")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    print(__doc__)
     main()
-
-
-
+    cv.destroyAllWindows()
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Loading complete
