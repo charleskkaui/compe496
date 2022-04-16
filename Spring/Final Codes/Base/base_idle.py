0456de9786bf640b1b18ba281692f1a769ff9432
charleskkaui <60274453+charleskkaui@users.noreply.github.com>
2022-04-15 18:39:09 -0700

    asldjfhaofh

--- Spring/Final Codes/Base/base_idle.py
+++ Spring/Final Codes/Base/base_idle.py
@@ -1,32 +1,28 @@
 import sys
 from bluedot.btcomm import BluetoothServer
 import time
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
+#import RPi.GPIO as GPIO
+#GPIO.setmode(GPIO.BCM)
 
-#GLOBAL_VARIABLES
-global received
 
-#INSTANTIATE BLUETOOTH
-s = BluetoothServer(data_received)
 
-#PIN_DECLERATIONS
-SENSOR_01 = 20
-SENSOR_02 = 21
-RELAY_01 = 14 #Latch 1
-RELAY_02 = 25  #Latch 2
-RELAY_03 = 12 #Latch 3
-RELAY_04 = 15 #Latch 4
+##PIN_DECLERATIONS
+#SENSOR_01 = 20
+#SENSOR_02 = 21
+#RELAY_01 = 14 #Latch 1
+#RELAY_02 = 25  #Latch 2
+#RELAY_03 = 12 #Latch 3
+#RELAY_04 = 15 #Latch 4
 
-#GPIO Initializations
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(SENSOR_01, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #set pin GPIO 17 to be input pin and set initial value to be pulled low(off)
-GPIO.setup(SENSOR_02, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #set pin GPIO 17 to be input pin and set initial value to be pulled low(off)
-GPIO.setup(RELAY_01,GPIO.OUT)
-GPIO.setup(RELAY_02,GPIO.OUT)
-GPIO.setup(RELAY_03,GPIO.OUT)
-GPIO.setup(RELAY_04,GPIO.OUT)
+##GPIO Initializations
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setwarnings(False)
+#GPIO.setup(SENSOR_01, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #set pin GPIO 17 to be input pin and set initial value to be pulled low(off)
+#GPIO.setup(SENSOR_02, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #set pin GPIO 17 to be input pin and set initial value to be pulled low(off)
+#GPIO.setup(RELAY_01,GPIO.OUT)
+#GPIO.setup(RELAY_02,GPIO.OUT)
+#GPIO.setup(RELAY_03,GPIO.OUT)
+#GPIO.setup(RELAY_04,GPIO.OUT)
 
 def data_received(data):
     print(data)
@@ -51,16 +47,25 @@ def unlatch():
     GPIO.output(RELAY_04,1)
 
 def main():
+    
+    #DEFINE GLOBAL VARIABLES
+    global s 
+    global received 
+
+    #INSTANTIATE BLUETOOTH
+    s = BluetoothServer(data_received)
+    
     received = "0"
+    
     while True:
         #COPY CODE TO CHECK SENSORS HERE
         ################################
         ################################
 
-        if "A" in received: #WE ARE LATCHING
+        if (received == "A"): #WE ARE LATCHING
             latch()
             received = "0"
-        elif "B" in received:
+        elif (received == "B"):
             unlatch()
             received = "0"
         else:
