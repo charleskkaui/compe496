210c77727f27827260cabfd9a69f35c6f797fe67
charleskkaui <60274453+charleskkaui@users.noreply.github.com>
2022-04-15 17:09:17 -0700

    fixed the base code

--- Spring/Final Codes/Base/base_idle.py
+++ Spring/Final Codes/Base/base_idle.py
@@ -10,23 +10,23 @@ global received
 #INSTANTIATE BLUETOOTH
 s = BluetoothServer(data_received)
 
-##PIN_DECLERATIONS
-#SENSOR_01 = 20
-#SENSOR_02 = 21
-#RELAY_01 = 16 #Latch 1
-#RELAY_02 = 4  #Latch 2
-#RELAY_03 = 12 #Latch 3
-#RELAY_04 = 13 #Latch 4
+#PIN_DECLERATIONS
+SENSOR_01 = 20
+SENSOR_02 = 21
+RELAY_01 = 14 #Latch 1
+RELAY_02 = 25  #Latch 2
+RELAY_03 = 12 #Latch 3
+RELAY_04 = 15 #Latch 4
 
-##GPIO Initializations
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setwarnings(False)
-#GPIO.setup(SENSOR_01, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #set pin GPIO 17 to be input pin and set initial value to be pulled low(off)
-#GPIO.setup(SENSOR_02, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #set pin GPIO 17 to be input pin and set initial value to be pulled low(off)
-#GPIO.setup(RELAY_01,GPIO.OUT)
-#GPIO.setup(RELAY_02,GPIO.OUT)
-#GPIO.setup(RELAY_03,GPIO.OUT)
-#GPIO.setup(RELAY_04,GPIO.OUT)
+#GPIO Initializations
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(SENSOR_01, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #set pin GPIO 17 to be input pin and set initial value to be pulled low(off)
+GPIO.setup(SENSOR_02, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #set pin GPIO 17 to be input pin and set initial value to be pulled low(off)
+GPIO.setup(RELAY_01,GPIO.OUT)
+GPIO.setup(RELAY_02,GPIO.OUT)
+GPIO.setup(RELAY_03,GPIO.OUT)
+GPIO.setup(RELAY_04,GPIO.OUT)
 
 def data_received(data):
     print(data)
@@ -36,20 +36,19 @@ def data_received(data):
 
 def latch():
     print("I am Latching")
-    #GPIO.output(4,0)
-    #GPIO.output(12,0)
-    #time.sleep(1)
-    #GPIO.output(16,1)
-    #GPIO.output(13,1)
+    GPIO.output(RELAY_01,0)
+    GPIO.output(RELAY_02,0)
+    time.sleep(1)
+    GPIO.output(RELAY_03,1)
+    GPIO.output(RELAY_04,1)
 
 def unlatch():
     print("I am Unlatching")                    
-    #GPIO.output(16,0)            
-    #GPIO.output(13,0)            
-    #time.sleep(1)
-    #GPIO.output(4,1)             
-    #GPIO.output(12,1)
-
+    GPIO.output(RELAY_01,0)            
+    GPIO.output(RELAY_02,0)            
+    time.sleep(1)
+    GPIO.output(RELAY_03,1)             
+    GPIO.output(RELAY_04,1)
 
 def main():
     received = "0"
