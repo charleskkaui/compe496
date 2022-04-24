dbbbdbb95ad934c6a32a9c7b9e0cf3ae092ff66e
charleskkaui <60274453+charleskkaui@users.noreply.github.com>
2022-04-23 23:09:41 -0700

    sdfghj

--- Spring/Test_Codes/Integrated/integration_02.py
+++ Spring/Test_Codes/Integrated/integration_02.py
@@ -114,7 +114,7 @@ def main():
 
     vehicle = connect_drone(CONNECTION_STRING,CONNECTION_BAUDRATE)
     arm(vehicle)
-    take_off_now(vehicle,2)   
+    take_off_now(vehicle,3)   
     #vehicle.mode = "LOITER"
     fly_go(vehicle,0,0,0,1)
     fly_spin(vehicle,0,True)
