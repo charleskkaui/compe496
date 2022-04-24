0b5b374ec3bc7eb0f2f2ba9081ffbed568874ff7
charleskkaui <60274453+charleskkaui@users.noreply.github.com>
2022-04-23 23:41:34 -0700

    dfghj

--- Spring/Test_Codes/Integrated/integration_02.py
+++ Spring/Test_Codes/Integrated/integration_02.py
@@ -146,6 +146,7 @@ def main():
     #vehicle.mode = "LOITER"
     fly_go(vehicle,0,0,0,1)
     fly_spin(vehicle,0,True)
+    time.sleep(5)
     vehicle.simple_goto(vehicle.home_location)
     
     #creating file for data
