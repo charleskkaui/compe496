7658266e5c162f2c654d1bdc4a99425dab1d938a
charleskkaui <60274453+charleskkaui@users.noreply.github.com>
2022-04-29 19:43:41 -0700

    started manual landing script for auto latching on disarm

---
+++ Spring/Final_Codes/Drone/drone_alt_land.py
@@ -0,0 +1,38 @@
+import sys, time, math, argparse, os.path, cv2
+import numpy as np
+import cv2.aruco as aruco
+from datetime import datetime
+from dronekit import connect, Command, LocationGlobal
+from pymavlink import mavutil
+from signal import pause
+
+#Establishes connection to the drone.
+def connect_drone(CONNECTION_STRING,CONNECTION_BAUDRATE):
+        print("CONNECTING...")
+        return connect(CONNECTION_STRING, wait_ready=True, baud=CONNECTION_BAUDRATE)
+
+#Callback function for the THR_MIN parameter
+def armed_callback(self, attr_name, value):
+    print(" PARAMETER CALLBACK: %s changed to: %s" % (attr_name, self.armed))
+
+
+
+
+def main():
+    #CONSTANTS_DRONE
+    CONNECTION_BAUDRATE = 57600
+    CONNECTION_STRING = '/dev/ttyAMA1'
+
+    vehicle = connect_drone()
+    
+    #Add observer for the vehicle's armed parameter
+    vehicle.parameters.add_attribute_listener('armed', armed_callback)
+
+
+    pause()
+
+
+
+if __name__ == "__main__":
+    main()
+        
