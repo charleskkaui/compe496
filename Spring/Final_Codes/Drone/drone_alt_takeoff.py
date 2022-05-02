bd022079d799d58192b04c5fde849157262c8ec6
charleskkaui <60274453+charleskkaui@users.noreply.github.com>
2022-05-01 21:27:26 -0700

    added necesarry functions for take off

--- Spring/Final_Codes/Drone/drone_alt_takeoff.py
+++ Spring/Final_Codes/Drone/drone_alt_takeoff.py
@@ -45,6 +45,41 @@ def arm(vehicle):
         time.sleep(1)
     print("DRONE ARMED")
 
+#Referenced from Dronekit-Python
+def fly_go(vehicle,velocity_x, velocity_y, velocity_z, duration):
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,
+        0, 0,
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
+        0b0000111111000111, # type_mask (only speeds enabled)
+        0, 0, 0,
+        velocity_x, velocity_y, velocity_z, # x, y, z velocity in m/s
+        0, 0, 0,
+        0, 0)
+
+    for x in range(0,duration):
+        vehicle.send_mavlink(msg)
+        time.sleep(1)
+
+#Referenced from Dronekit-Python
+def fly_spin(vehicle,heading, relative=False):
+    if relative:
+        is_relative=1 #yaw relative to direction of travel
+    else:
+        is_relative=0 #yaw is an absolute angle
+    # create the CONDITION_YAW command using command_long_encode()
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+        0, #confirmation
+        heading,    # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        1,          # param 3, direction -1 ccw, 1 cw
+        is_relative, # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)    # param 5 ~ 7 not used
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+
 
 def take_off_now(vehicle,TargetAltitude):
     print("Ascending...")
