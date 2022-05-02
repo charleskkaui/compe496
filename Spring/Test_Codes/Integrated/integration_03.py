6fb8fa2012e9b08485e35525190c78a4f5b3dbc8
charleskkaui <60274453+charleskkaui@users.noreply.github.com>
2022-05-02 00:04:31 -0700

    dfghjk

--- Spring/Test_Codes/Integrated/integration_03.py
+++ Spring/Test_Codes/Integrated/integration_03.py
@@ -110,7 +110,7 @@ def main():
     #CONSTANTS_DRONE
     CONNECTION_BAUDRATE = 57600
     CONNECTION_STRING = '/dev/ttyAMA1' #COnnection String for the Drone
-    VELOCITY = 0.3
+    VELOCITY = 1
     ALTITUDE_TAKEOFF = 1
     #CONSTANTS_ARUCO_POSITION
     TARGET_X = 8
